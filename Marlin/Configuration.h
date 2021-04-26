a65efc19d4775f3eee361fc5d9036cd1d66f1840
amonra50 <dscott598@gmail.com>
2021-04-25 21:47:02 -0400

    Update Configuration.h

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -499,9 +499,9 @@
     #define DEFAULT_Ki_LIST {   1.08,   1.08 }
     #define DEFAULT_Kd_LIST { 114.00, 114.00 }
   #else
-    #define DEFAULT_Kp  21.3
-    #define DEFAULT_Ki  1.79
-    #define DEFAULT_Kd  63.6
+    #define DEFAULT_Kp  21.63
+    #define DEFAULT_Ki  1.73
+    #define DEFAULT_Kd  67.49
   #endif
 #endif // PIDTEMP
 
